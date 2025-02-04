c4f8af724c4baa9c77ce0f7352f168b3f5605e12
tsunderek96 <67269558+tsunderek96@users.noreply.github.com>
2025-02-04 19:55:15 +0800

    Script.js

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ const yesButton = document.getElementById('yes');
 const congratsMessage = document.getElementById('congrats');
 
 noButton.addEventListener('mouseover', () => {
-    const maxX = window.innerWidth - noButton.offsetWidth;
-    const maxY = window.innerHeight - noButton.offsetHeight;
+    const maxX = window.innerWidth - noButton.offsetWidth - 20;
+    const maxY = window.innerHeight - noButton.offsetHeight - 20;
     const randomX = Math.random() * maxX;
     const randomY = Math.random() * maxY;
+
     noButton.style.position = 'absolute';
     noButton.style.left = `${randomX}px`;
     noButton.style.top = `${randomY}px`;
